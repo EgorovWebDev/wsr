c2a06814e5b78b900c41b9d6756afa55fc238766
Eduard <egedw@mail.ru>
2018-09-29 10:25:36 +0300

    Wsr is my hell

---
+++ task_3/src/js/main.js
@@ -0,0 +1,11 @@
+
+let menu = document.getElementsByClassName('menu')[0];
+let nav = document.getElementsByTagName('nav')[0];
+
+    menu.onclick = function() {
+        menu.classList.toggle("menu-active");
+    };
+
+    menu.onclick = function() {
+        nav.classList.toggle("active");
+    }
